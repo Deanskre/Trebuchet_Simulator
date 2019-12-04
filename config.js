3df85e9cbae904f8dce636090605197e3ff66af7
Dean <dareiter02@gmail.com>
2019-12-03 16:50:31 -0800

    bmbkbkhj

--- config.js
+++ config.js
@@ -3,27 +3,8 @@
 
 var GAME = {
   canvas : {
-    width : 1200,
-    height : 500
+    width : 1600,
+    height : 900
   },
-  started : true,
-  level : 1,
-  t : 0,//this and the next two affect the rotation of the catapult
-  tv: .05,
-  ta: .001
+  started : true
 };
-
-var TREBUCHET = {
-  counterweight: 10, //affects size of force on projectile
-  releaseAngle: (Math.PI)/2.7 //affects angle that force makes with the horizontal
-}
-
-var PROJ ={
-  x: 17,
-  y: 463,
-  rad: 5, //radius of projectile
-  xv:0, //xspeed
-  yv: 0, //yspeed
-  ya: .2, //gravity
-  fired : false
-}
